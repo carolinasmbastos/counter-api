a7575bce4b9790a2e5e333dbd32bc810293600e1
carolinasmbastos <carolinasmbastos@gmail.com>
2020-02-26 14:56:12 -0800

    Create /next endpoint

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const app = express();
 
+const prefix = '/api'
+var counter = 0;
+
+// endpoint: /next
+// method: get
+// retrieves the next available integer value (greater than 0)
+app.get(prefix + '/next', (req, res) => {
+    res.json({data : {
+        type : "integer",
+        id : ++counter
+    }})
+})
 
 app.set('port', process.env.PORT || 8080);
 
