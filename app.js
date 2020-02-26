5ddecf04de79dbc00aad261aed2b269dcc592f67
carolinasmbastos <carolinasmbastos@gmail.com>
2020-02-26 14:23:55 -0800

    Initial setup

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+
+app.set('port', process.env.PORT || 8080);
+
+const server = app.listen(app.get('port'), () => {
+    console.log("Server listening on ", app.get('port'))
+})
