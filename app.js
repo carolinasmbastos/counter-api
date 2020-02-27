2196eca0f22f63748fa2f3558e05dd777082079d
carolinasmbastos <carolinasmbastos@gmail.com>
2020-02-26 16:09:35 -0800

    Create /current PUT endpoint

--- app.js
+++ app.js
@@ -1,33 +1,64 @@
 const express = require("express");
 const app = express();
+const { validationResult } = require("express-validator");
+const { validateCounter } = require("./validators/validateCounter");
 
-const prefix = '/api'
+bodyParser = require("body-parser");
+const cors = require("cors");
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+const prefix = "/api";
 var counter = 0;
 
-// endpoint: /next
-// method: get
+app.use(express.urlencoded({ extended: true }));
+
+// endpoint: /next, method: get
 // retrieves the next available integer value (greater than 0)
-app.get(prefix + '/next', (req, res) => {
-    res.json({data : {
-        type : "integer",
-        id : ++counter
-    }})
-})
+app.get(prefix + "/next", (req, res) => {
+  res.json({
+    data: {
+      type: "integer",
+      id: ++counter
+    }
+  });
+});
 
-
-// endpoint: /current
-// method: get
+// endpoint: /current, method: get
 // retrieves the current available integer value (greater than 0)
-app.get(prefix + '/current', (req, res) => {
-    res.json({data : {
-        type : "integer",
-        id : counter
-    }})
-})
+app.get(prefix + "/current", (req, res) => {
+  res.json({
+    data: {
+      type: "integer",
+      id: counter
+    }
+  });
+});
 
+// endpoint: /current, method: put
+// reset the current value to a given non-negative value
+app.put(prefix + "/current", validateCounter, (req, res) => {
+  //Get validation results if there are any
+  const valErrors = validationResult(req).array();
 
-app.set('port', process.env.PORT || 8080);
+  if (valErrors.length != 0) {
+    console.log(valErrors);
+    res.status(422).send({ errors: valErrors });
+  } else {
+    counter = req.body.current;
+    res.json({
+      data: {
+        type: "integer",
+        id: counter
+      }
+    });
+  }
+});
 
-const server = app.listen(app.get('port'), () => {
-    console.log("Server listening on ", app.get('port'))
-})
+app.set("port", process.env.PORT || 8080);
+
+const server = app.listen(app.get("port"), () => {
+  console.log("Server listening on ", app.get("port"));
+});
---
+++ validators/validateCounter.js
@@ -0,0 +1,10 @@
+const { body } = require("express-validator");
+
+exports.validateCounter = [
+  body("current", "New value cannot be empty").exists(),
+  body("current", "New value must be an integer").isInt(),
+  body("current", "New value must positive").custom((value, { req }) => {
+    if (value < 0) throw new Error("New value must be a positive integer");
+    return true;
+  })
+];
